ebeebdd532847a85bb45cb8dec5016273f0278f0
alixlm19 <alixlm19@hotmail.com>
2022-11-14 17:36:15 -0500

    Added Thread data model

--- src/blueprints/thread.py
+++ src/blueprints/thread.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, jsonify
 from dbservice import thread_repository, comment_repository
+from models.thread import Thread
 
 bp = Blueprint("thread", __name__, url_prefix="/thread")
 
@@ -8,6 +9,8 @@ bp = Blueprint("thread", __name__, url_prefix="/thread")
 def get_thread_detail(id):
     thread = thread_repository.get_thread_by_id(id)
     comments = comment_repository.find_main_comment_by_thread(id)
+    
+    thread = Thread(thread)
 
     # todo: Comment actually has a recursive structure (comment - subcomment - subsub comment...)
     # todo: How to recursively rendering sub comments with Jinja?
