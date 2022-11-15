712244207800a89e9078f8137eb36a75581725a9
alixlm19 <alixlm19@hotmail.com>
2022-11-15 02:21:19 -0500

    Added thread replies

--- src/blueprints/thread.py
+++ src/blueprints/thread.py
@@ -1,12 +1,13 @@
-from flask import Blueprint, render_template, jsonify
+from flask import Blueprint, render_template, jsonify, request, redirect, url_for, session
 from dbservice import thread_repository, comment_repository
 from models.thread import Thread
 from models.comment import Comment
 
+
 import os
 
 relative_static_folder = os.environ.get("STATIC_FOLDER")
-static_folder = os.path.abspath('../frontend/assets')
+static_folder = os.path.abspath(relative_static_folder)
 
 bp = Blueprint(
     "thread",
@@ -23,7 +24,16 @@ def get_thread_detail(id):
 
     # comments = Comment.comment_list(comments)
     comments = Comment.get_comment_by_thread_id(id)
-    print(comments)
     # todo: Comment actually has a recursive structure (comment - subcomment - subsub comment...)
     # todo: How to recursively rendering sub comments with Jinja?
     return render_template("thread.html", thread=thread, comments=comments)
+
+@bp.route("/<int:id>", methods=['POST'])
+def add_comment(id):
+    thread_id = id
+    comment = request.form.get('comment')
+    reply_id = request.form.get('reply_id', "null")
+    user_id = session.get('user').get("id")
+
+    comment_repository.add_comment(comment, user_id, thread_id, reply_id)
+    return redirect(url_for('thread.get_thread_detail', id=id))
